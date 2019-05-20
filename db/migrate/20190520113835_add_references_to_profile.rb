5b8978ef6c8501b79831b765f79957465522b6ed
Matthew Bates <matbat99@yahoo.com>
2019-05-20 13:42:43 +0200

    added profile references to user

---
+++ db/migrate/20190520113835_add_references_to_profile.rb
@@ -0,0 +1,5 @@
+class AddReferencesToProfile < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :profiles, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_113408) do
+ActiveRecord::Schema.define(version: 2019_05_20_113835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2019_05_20_113408) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -47,4 +49,5 @@ ActiveRecord::Schema.define(version: 2019_05_20_113408) do
   end
 
   add_foreign_key "events", "users"
+  add_foreign_key "profiles", "users"
 end
