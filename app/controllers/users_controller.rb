5641edb5ab0de3cefebc22e83402123149d127dd
Zachary Kwiecinski <Zachary.Kwiecinski@gmail.com>
2019-05-20 16:37:46 +0200

    allow user show to access profile data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @hosted_events = Event.where(user_id: @user)
+    @profile = @user.profile
     # @booked_events = @user.bookings # this line is for finding all the events a user is booked for
-    # @profile = Profile.find(@user)# this is for getting the profile information accessable to the view
   end
 end
