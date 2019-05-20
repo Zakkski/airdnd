5eb7be9b314780d1a1c352769b86541b769a2cb0
Zachary Kwiecinski <Zachary.Kwiecinski@gmail.com>
2019-05-20 15:31:29 +0200

    Add user show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :events, only: [:new, :create]
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
