4c5eabb8a1a0d15fc83a642c544e75040f1cc149
Matthew Bates <matbat99@yahoo.com>
2019-05-21 22:09:18 +0200

    address autocomplete and form styling

---
+++ app/javascript/components/address_autocomplete.js
@@ -0,0 +1,8 @@
+const places = require('places.js');
+const placesAutocomplete = places({
+  appId: 'plBT9OXXW7UZ',
+  apiKey: '9e5e1cab79398b605c8c1b58b963894e',
+  container: document.querySelector('#event_location')
+});
+
+
