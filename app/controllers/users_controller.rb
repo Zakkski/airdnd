82e5f67d8775de37cd2e2b1f8912425ff8a37b97
Zachary Kwiecinski <Zachary.Kwiecinski@gmail.com>
2019-05-21 14:35:12 +0200

    create permission for user to see their profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+    authorize @user
     @hosted_events = Event.where(user_id: @user)
     @profile = @user.profile
     # @booked_events = @user.bookings # this line is for finding all the events a user is booked for
