199189397bfa2b2bd7d76d144caa660c88f3a1cf
Matthew Bates <matbat99@yahoo.com>
2019-05-22 12:23:12 +0200

    geocode and map

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,10 @@
 class Event < ApplicationRecord
   belongs_to :user
+  geocoded_by :location
   validates :game, :description, :date, :location, presence: true
+  after_validation :geocode, if: :will_save_change_to_location?
 
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
+
 end
